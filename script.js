3c4150872fa5778f3ded4aafdefac57e02ad5cba
B1aho <wheelsurgut00@gmail.com>
2024-10-11 14:25:51 +0300

    Add comments and clear the code

--- script.js
+++ script.js
@@ -20,42 +20,43 @@ function addBookToLibrary(id, title, author, pages, haveRead) {
     return book
 }
 
-function libraryList() {
-    myLibrary.forEach((book, idx) => {
-        console.log(idx + ') ' + book.bookInfo() + '\n')
-    })
-}
-
 // html-elements
 const dialog = document.querySelector("#dialog-book")
 const form = document.querySelector("#book-form")
 const openDialogBtn = document.querySelector(".main-btn")
 const booksContainer = document.querySelector("#book-cards")
 
+
+// on button click show dialog popup
+openDialogBtn.addEventListener('click', openDialog)
+
 function openDialog() {
     dialog.showModal()
 }
 
+/* 
+When form is submit, serialize data from form into one object, give id to it and add to library and emit event when done
+*/
+form.addEventListener('submit', submitHandle)
+
 function serializeForm() {
     return Array.from(form.elements).filter((item) => {
-        if (!!item.name)
-            if (item.type === 'radio' && !item.checked)
-                return false
-            else
-                return true
+        // Take only named elements of form (exclude button) 
+       if (item.name) return true
     }).map((item) => {
+        // Map every named element of form and transform it into object with name and value props. Result is array of such objects.
         const { name, type } = item
         const value = type === 'checkbox' && item.checked ? true : type === 'checkbox' ? false : item.value
         return { name, value }
+        // Reduce array of objects into one object like Book type
     }).reduce((obj, curr) => {
         obj[curr.name] = curr.value
         return obj
     }, {})
 }
 
-
 function submitHandle() {
-    input = serializeForm()
+    let input = serializeForm()
     const book = addBookToLibrary(Date.now(), input.title, input.author, input.pages, input.haveRead)
     form.reset()
     let event = new Event('addBook')
@@ -63,22 +64,22 @@ function submitHandle() {
     form.dispatchEvent(event)
 }
 
-openDialogBtn.addEventListener('click', openDialog)
-form.addEventListener('submit', submitHandle)
+// When book was add to library, show card of recently added book
 form.addEventListener('addBook', showBookCard)
 
 function showBookCard(e) {
     const book = e.value
-    // Даём карточке такой же айдишник, как и у книги
-    const idx = myLibrary[myLibrary.indexOf(book)].id
+    // Give card-element same book's id and append card
     const card = document.createElement("div")
+    const id = myLibrary[myLibrary.indexOf(book)].id
     card.className = "book-card"
-    card.setAttribute('id', `card${idx}`)
+    card.setAttribute('id', `card${id}`)
     booksContainer.append(card)
-    for (prop in book) {
+    for (let prop in book) {
+        // Iterate only through book's own properties
         if (Object.prototype.hasOwnProperty.call(book, prop)) {
+            // Add 'Read' btn depends on haveRead value
             if (prop === 'haveRead') {
-                // Add read btn
                 const readBtn = document.createElement("button")
                 readBtn.addEventListener("click", changeReadStatus)
                 readBtn.className = "read"
@@ -99,21 +100,21 @@ function showBookCard(e) {
             }
         }
     }
-    // Add remove btn on card display
+    // Add remove btn on card's display
     const removeBtn = document.createElement("button")
     removeBtn.className = "remove"
-    removeBtn.setAttribute('index', +idx)
+    removeBtn.id = +id
     removeBtn.addEventListener('click', removeCard)
     removeBtn.innerText = "Remove"
     card.append(removeBtn)
 }
 
 function removeCard(e) {
-    const idx =  e.target.getAttribute('index')
+    const id = e.target.id
     myLibrary = myLibrary.filter((item) => {
-       return item.id.toString() !== idx
+       return item.id.toString() !== id
     })
-    const card = document.querySelector(`#card${idx}`)
+    const card = document.querySelector(`#card${id}`)
     card.remove()
 }
 
